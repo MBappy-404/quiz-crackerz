e8c1e089dd4c5591221dfbcca0421f2a4c061938
MBappy-404 <muhammadbappy989@gmail.com>
2022-10-11 19:24:18 +0530

    quize section update

--- src/components/ShowQuestion/ShowQuestion.js
+++ src/components/ShowQuestion/ShowQuestion.js
@@ -1,40 +1,58 @@
+import { useState } from 'react';
 import { Button, Card } from 'react-bootstrap';
+import { toast } from 'react-toastify';
 import './ShowQuestion.css'
-
+import { EyeFill } from 'react-bootstrap-icons';
 const ShowQuestion = ({ quiz }) => {
 
      const { correctAnswer, question, options } = quiz;
+     const [answer, setAnswer] = useState([]);
+   
 
-   console.log(correctAnswer);
-    
-     
+     console.log(correctAnswer);
+
+
      const handleBreakTime = (event) => {
-          const correct =(event.target.innerText);
+          const correct = (event.target.innerText);
 
-          if( correct === correctAnswer){
-               alert('success')
-          
-          
+          if (correct === correctAnswer) {
+               // alert('success')
+               toast.success('Correct Answer', { autoClose: 1500 })
+                
+
           }
-          else{
-               alert('wrong')
+          else {
+               toast.error('Wrong Answer', { autoClose: 1500 })
           }
-       
+
+
+     }
+
+     const showAnswer = () => {
+          const correct = correctAnswer;
+          setAnswer(correct);
+
+          toast.success(<h5>{correct}</h5>,{ autoClose: 3500 });
+
 
      }
 
 
-   
+
      return (
           <div>
-               <Card className="text-center mb-5">
-                    <Card.Header className=''> <h4>Quiz : {question.replace(/(<([^>]+)>)/ig, '')}</h4> </Card.Header>
+               <Card className="text-center m-3 mb-5">
+                    <Card.Header className='d-flex justify-content-between'>
+                          <h4>Quiz : {question.replace(/(<([^>]+)>)/ig, '')}</h4>
+                          <Button onClick={showAnswer} variant='light'><EyeFill color="blue" size={20} /></Button>
+                           </Card.Header>
                     <Card.Body className=''>
-                         
-                      {
-                         options.map( option => <Button onClick={(event) => handleBreakTime(event)} md={6} className='w-75 m-auto p-4  mb-2' variant="primary"> {option}</Button>
-                         )
-                      }
+
+                         {
+                              options.map(option => <Button onClick={(event) => handleBreakTime(event)} md={6} className='w-75 m-auto p-4  mb-2' variant="primary"> {option}</Button>
+
+                              )
+                         }
 
                     </Card.Body>
                     <Card.Footer className="text-muted">2 days ago</Card.Footer>
